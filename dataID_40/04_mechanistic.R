9f208d99357e6b54c3a1f4cdcbb745bf3ea9b30d
Umar Niazi <uhkniazi@gmail.com>
2019-08-15 13:49:07 +0100

    using utilities in rethinking package to plot various combinations of covariates

---
+++ dataID_40/04_mechanistic.R
@@ -0,0 +1,171 @@
+# File: 04_mechanistic.R
+# Auth: umar.niazi@kcl.ac.uk
+# DESC: some tests for mechanistic DAGs
+# Date: 14/08/2019
+
+
+source('header.R')
+setwd('dataID_40/')
+
+load('temp/dfData.pa.rds')
+str(dfData.pa)
+
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+#devtools::install_github("rmcelreath/rethinking", ref = "Experimental" )
+library(rethinking)
+########################################################
+###### univariate models
+########################################################
+## choose the variable to model
+colnames(dfData.pa)
+cVar = 'Peanut.Sp.Act'
+##### fit a lm
+fit.1.pa = lm(CD63.Act ~ ., data=dfData.pa[, c(cVar, 'CD63.Act')])
+summary(fit.1.pa)
+
+fit.1 <- quap(
+  alist(
+    CD63.Act ~ dnorm(mu, sigmaPop),
+    mu <- b0 + b1*Peanut.Sp.Act,
+    b0 ~ dcauchy(0, 2),
+    b1 ~ dcauchy(0, 2),
+    sigmaPop ~ dexp(1)
+  ), data=dfData.pa
+)
+summary(fit.1)
+
+stanDso = rstan::stan_model(file='tResponseRegression_censored.stan')
+
+## model dataset PA
+m = model.matrix(CD63.Act ~ ., data=dfData.pa[dfData.pa$CD63.Act > 0.01, c(cVar, 'CD63.Act')])
+m2 = model.matrix(CD63.Act ~ ., data=dfData.pa[dfData.pa$CD63.Act <= 0.01, c(cVar, 'CD63.Act')])
+
+lStanData = list(Ntotal=nrow(m), Ncol=ncol(m), X=m, rLower=0.01,
+                 X2 = m2,
+                 Ncens = nrow(m2),
+                 y=dfData.pa$CD63.Act[dfData.pa$CD63.Act > 0.01])
+
+fit.stan.pa = sampling(stanDso, data=lStanData, iter=2000, chains=2, pars=c('betas', 'mu', 'y_cens', 'mu2', 'sigmaPop', 'nu'),
+                       cores=2)
+
+################# model checks
+print(fit.stan.pa, c('betas', 'sigmaPop', 'nu'), digits=3)
+
+traceplot(fit.stan.pa, c('betas'))
+
+## testing some summary functions from rethinking package
+post = extract.samples(fit.1, n=1000)
+dim(post)
+head(post)
+precis(post)
+
+betas.pa = extract(fit.stan.pa, 'betas')$betas
+colnames(betas.pa) = c('b0', 'b1')
+dim(betas.pa)
+precis(as.data.frame(betas.pa))
+
+plot(CD63.Act ~ dfData.pa[,cVar], data=dfData.pa, xlab=cVar)
+curve(mean(betas.pa[,'b0']) + mean(betas.pa[,'b1']) * x, add=T)
+abline(a = mean(betas.pa[,'b0']), b= mean(betas.pa[,'b1']), col='red')
+
+i = range(dfData.pa[,cVar])
+iGrid = seq(i[1], i[2], length.out = 50)
+mGrid = cbind(1, iGrid)
+mFitted = t(mGrid %*% t(betas.pa))
+dim(mFitted)
+
+## posterior predictive values for fitted
+lines(iGrid, colMeans(mFitted), col='green')
+mu.hpdi = apply(mFitted, 2, HPDI)
+shade(mu.hpdi, iGrid)
+
+############### new simulated data
+###############
+### generate some posterior predictive data
+## generate random samples from alternative t-distribution parameterization
+## see https://grollchristian.wordpress.com/2013/04/30/students-t-location-scale/
+rt_ls <- function(n, df, mu, a) rt(n,df)*a + mu
+## follow the algorithm in section 14.3 page 363 in Gelman 2013
+simulateOne = function(betas, sigma, nu, mModMatrix){
+  f = mModMatrix %*% betas
+  yrep = rt_ls(length(f), nu, f,  sigma)
+  # censor the values below detection limit
+  yrep[yrep <= 0.01] = 0.01
+  return(yrep)
+}
+
+## sample n values, 1000 times
+mDraws.sim = matrix(NA, nrow = length(iGrid), ncol=300)
+l = extract(fit.stan.pa)
+for (i in 1:300){
+  p = sample(1:nrow(l$betas), 1)
+  mDraws.sim[,i] = simulateOne(l$betas[p,], 
+                               l$sigmaPop[p],
+                               l$nu[p],
+                               mGrid)
+}
+
+dim(mDraws.sim)
+mDraws.PI = apply(mDraws.sim, 1, PI)
+shade(mDraws.PI, iGrid)
+apply(mDraws.sim, 2, function(x) lines(lowess(iGrid, x), lwd=0.5, col='red'))
+#apply(mDraws.sim, 2, function(x) points(iGrid, x, cex=0.5, pch=20, col='red'))
+###############
+
+# compare with lm 
+coef(fit.1.pa); colMeans(betas.pa); coef(fit.1)
+
+s2 = cbind(extract(fit.stan.pa)$betas)
+colnames(s2) = c(colnames(lStanData$X))
+dim(s2)
+pairs(s2[sample(1:nrow(s2), 1000),], pch=20, col='grey', cex=0.5, main='PA')
+
+########################################################
+dfData.pa.bk = dfData.pa
+dfData.pa = dfData.pa[dfData.pa$CD63.Act > 0.01,]
+
+######## various model sizes
+fit.1 <- quap(
+  alist(
+    CD63.Act ~ dnorm(mu, sigmaPop),
+    mu <- b0 + b1*Peanut.Sp.Act,
+    b0 ~ dcauchy(0, 2),
+    b1 ~ dcauchy(0, 2),
+    sigmaPop ~ dexp(1)
+  ), data=dfData.pa,
+  start=list(b0=21)
+)
+summary(fit.1)
+
+fit.2 <- quap(
+  alist(
+    CD63.Act ~ dnorm(mu, sigmaPop),
+    mu <- b0 + b2*Age,
+    b0 ~ dcauchy(0, 2),
+    b2 ~ dcauchy(0, 2),
+    sigmaPop ~ dexp(1)
+  ), data=dfData.pa,
+  start=list(b0=21, b2=0)
+)
+summary(fit.2)
+
+fit.3 <- quap(
+  alist(
+    CD63.Act ~ dnorm(mu, sigmaPop),
+    mu <- b0 + b1*Peanut.Sp.Act + b2*Age,
+    b0 ~ dcauchy(0, 2),
+    c(b1, b2) ~ dcauchy(0, 2),
+    sigmaPop ~ dunif(1, 40)
+  ), data=dfData.pa,
+  start=list(b0=21, b1=0, b2=0)
+)
+summary(fit.3)
+
+plot(coeftab(fit.1, fit.2, fit.3), pars=c('b1', 'b2'))
+
+dfData.pa = dfData.pa.bk
+
+
