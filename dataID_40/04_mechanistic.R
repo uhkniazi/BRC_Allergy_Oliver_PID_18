a94f137785193b9b013baf0e8af09a3787ac638d
Umar Niazi <uhkniazi@gmail.com>
2019-08-20 14:52:52 +0100

    added the bivariate model section

--- dataID_40/04_mechanistic.R
+++ dataID_40/04_mechanistic.R
@@ -21,7 +21,7 @@ library(rethinking)
 ########################################################
 ## choose the variable to model
 colnames(dfData.pa)
-cVar = 'total.IgE'
+cVar = 'Ara.h.2.Sp.Act'
 ##### fit a lm
 fit.1.pa = lm(CD63.Act ~ ., data=dfData.pa[, c(cVar, 'CD63.Act')])
 summary(fit.1.pa)
@@ -29,7 +29,7 @@ summary(fit.1.pa)
 fit.1 <- quap(
   alist(
     CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*total.IgE,
+    mu <- b0 + b1*Ara.h.2.Sp.Act,
     b0 ~ dcauchy(0, 2),
     b1 ~ dnorm(0, 10),
     sigmaPop ~ dexp(1)
@@ -131,7 +131,7 @@ dfData.pa = dfData.pa[dfData.pa$CD63.Act > 0.01,]
 fit.1 <- quap(
   alist(
     CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*total.IgE,
+    mu <- b0 + b1*Ara.h.2.Sp.Act,
     b0 ~ dcauchy(0, 2),
     b1 ~ dnorm(0, 10),
     sigmaPop ~ dexp(1)
@@ -167,7 +167,7 @@ summary(fit.3)
 fit.4 <- quap(
   alist(
     CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*total.IgE + b2*ISAC.Shannon + b3*Peanut.Sp.Act,
+    mu <- b0 + b1*Ara.h.2.Sp.Act + b2*ISAC.Shannon + b3*Peanut.Sp.Act,
     b0 ~ dcauchy(0, 2),
     c(b1, b2, b3) ~ dnorm(0, 10),
     sigmaPop ~ dunif(1, 40)
@@ -179,7 +179,7 @@ summary(fit.4)
 fit.5 <- quap(
   alist(
     CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*total.IgE + b2*ISAC.Shannon, #+ b3*Peanut.Sp.Act,
+    mu <- b0 + b1*Ara.h.2.Sp.Act + b2*ISAC.Shannon, #+ b3*Peanut.Sp.Act,
     b0 ~ dcauchy(0, 2),
     c(b1, b2) ~ dnorm(0, 10),
     sigmaPop ~ dunif(1, 40)
@@ -191,7 +191,7 @@ summary(fit.5)
 fit.6 <- quap(
   alist(
     CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*total.IgE + b3*Peanut.Sp.Act,
+    mu <- b0 + b1*Ara.h.2.Sp.Act + b3*Peanut.Sp.Act,
     b0 ~ dcauchy(0, 2),
     c(b1, b3) ~ dnorm(0, 10),
     sigmaPop ~ dunif(1, 40)
@@ -217,25 +217,153 @@ plot(coeftab(fit.1, fit.2, fit.3, fit.4, fit.5, fit.6, fit.7), pars=c('b1', 'b2'
 
 cov2cor(vcov(fit.4))
 
-## simulate the DAGs
-# P.Sp.A -> P.Sh
-df.Sim = data.frame(1:100)
-df.Sim$Peanut.Sp.Act = rnorm(100)
-df.Sim$Peanut.Shannon = rnorm(100, df.Sim$Peanut.Sp.Act)
-df.Sim$ISAC.Shannon = rnorm(100)
-df.Sim$CD63.Act = rnorm(100, df.Sim$Peanut.Sp.Act + df.Sim$Peanut.Shannon + df.Sim$ISAC.Shannon)
-str(df.Sim)
-
-fit.5 <- quap(
-  alist(
-    CD63.Act ~ dnorm(mu, sigmaPop),
-    mu <- b0 + b1*Peanut.Shannon + b2*ISAC.Shannon + b3*Peanut.Sp.Act,
-    b0 ~ dcauchy(0, 2),
-    c(b1, b2, b3) ~ dnorm(0, 10),
-    sigmaPop ~ dunif(1, 40)
-  ), data=df.Sim,
-  start=list(b0=21, b1=0, b2=0, b3=0)
-)
-summary(fit.5)
-dfData.pa = df.Sim
 dfData.pa = dfData.pa.bk
+
+###################################
+####### multivariate censored model
+## model dataset PA
+cVar = c('ISAC.Shannon', 'Peanut.Sp.Act')
+m = model.matrix(CD63.Act ~ ., data=dfData.pa[dfData.pa$CD63.Act > 0.01, c(cVar, 'CD63.Act')])
+m2 = model.matrix(CD63.Act ~ ., data=dfData.pa[dfData.pa$CD63.Act <= 0.01, c(cVar, 'CD63.Act')])
+
+lStanData = list(Ntotal=nrow(m), Ncol=ncol(m), X=m, rLower=0.01,
+                 X2 = m2,
+                 Ncens = nrow(m2),
+                 y=dfData.pa$CD63.Act[dfData.pa$CD63.Act > 0.01])
+
+fit.stan.pa = sampling(stanDso, data=lStanData, iter=2000, chains=2, pars=c('betas', 'mu', 'y_cens', 'mu2', 'sigmaPop', 'nu'),
+                       cores=2)
+
+################# model checks
+print(fit.stan.pa, c('betas', 'sigmaPop', 'nu'), digits=3)
+
+traceplot(fit.stan.pa, c('betas'))
+
+betas.pa = extract(fit.stan.pa, 'betas')$betas
+colnames(betas.pa) = c('b0', 'b1', 'b2')
+dim(betas.pa)
+precis(as.data.frame(betas.pa))
+
+## set index of cVar to 1 or 2 depending on which variable is being modelled
+## the other can be zero as the data has been centered
+plot(CD63.Act ~ dfData.pa[,cVar[1]], data=dfData.pa, xlab=cVar[1])
+curve(mean(betas.pa[,'b0']) + mean(betas.pa[,'b1']) * x, add=T)
+abline(a = mean(betas.pa[,'b0']), b= mean(betas.pa[,'b1']), col='red')
+
+i = range(dfData.pa[,cVar[1]])
+iGrid = seq(i[1], i[2], length.out = 50)
+mGrid = cbind(1, iGrid, 0)
+mFitted = t(mGrid %*% t(betas.pa))
+dim(mFitted)
+
+## posterior predictive values for fitted
+lines(iGrid, colMeans(mFitted), col='green')
+mu.hpdi = apply(mFitted, 2, HPDI)
+shade(mu.hpdi, iGrid)
+
+############### new simulated data
+###############
+### generate some posterior predictive data
+## generate random samples from alternative t-distribution parameterization
+## see https://grollchristian.wordpress.com/2013/04/30/students-t-location-scale/
+rt_ls <- function(n, df, mu, a) rt(n,df)*a + mu
+## follow the algorithm in section 14.3 page 363 in Gelman 2013
+simulateOne = function(betas, sigma, nu, mModMatrix){
+  f = mModMatrix %*% betas
+  yrep = rt_ls(length(f), nu, f,  sigma)
+  # censor the values below detection limit
+  yrep[yrep <= 0.01] = 0.01
+  return(yrep)
+}
+
+## sample n values, 1000 times
+mDraws.sim = matrix(NA, nrow = length(iGrid), ncol=300)
+l = extract(fit.stan.pa)
+for (i in 1:300){
+  p = sample(1:nrow(l$betas), 1)
+  mDraws.sim[,i] = simulateOne(l$betas[p,], 
+                               l$sigmaPop[p],
+                               l$nu[p],
+                               mGrid)
+}
+
+dim(mDraws.sim)
+mDraws.PI = apply(mDraws.sim, 1, PI)
+shade(mDraws.PI, iGrid)
+apply(mDraws.sim, 2, function(x) lines(lowess(iGrid, x), lwd=0.5, col='red'))
+
+### repeat for the second covariate
+plot(CD63.Act ~ dfData.pa[,cVar[2]], data=dfData.pa, xlab=cVar[2])
+curve(mean(betas.pa[,'b0']) + mean(betas.pa[,'b2']) * x, add=T)
+abline(a = mean(betas.pa[,'b0']), b= mean(betas.pa[,'b2']), col='red')
+
+i = range(dfData.pa[,cVar[2]])
+iGrid = seq(i[1], i[2], length.out = 50)
+mGrid = cbind(1, 0, iGrid)
+mFitted = t(mGrid %*% t(betas.pa))
+dim(mFitted)
+
+## posterior predictive values for fitted
+lines(iGrid, colMeans(mFitted), col='green')
+mu.hpdi = apply(mFitted, 2, HPDI)
+shade(mu.hpdi, iGrid)
+
+############### new simulated data
+###############
+### generate some posterior predictive data
+## generate random samples from alternative t-distribution parameterization
+## see https://grollchristian.wordpress.com/2013/04/30/students-t-location-scale/
+rt_ls <- function(n, df, mu, a) rt(n,df)*a + mu
+## follow the algorithm in section 14.3 page 363 in Gelman 2013
+simulateOne = function(betas, sigma, nu, mModMatrix){
+  f = mModMatrix %*% betas
+  yrep = rt_ls(length(f), nu, f,  sigma)
+  # censor the values below detection limit
+  yrep[yrep <= 0.01] = 0.01
+  return(yrep)
+}
+
+## sample n values, 1000 times
+mDraws.sim = matrix(NA, nrow = length(iGrid), ncol=300)
+l = extract(fit.stan.pa)
+for (i in 1:300){
+  p = sample(1:nrow(l$betas), 1)
+  mDraws.sim[,i] = simulateOne(l$betas[p,], 
+                               l$sigmaPop[p],
+                               l$nu[p],
+                               mGrid)
+}
+
+dim(mDraws.sim)
+mDraws.PI = apply(mDraws.sim, 1, PI)
+shade(mDraws.PI, iGrid)
+apply(mDraws.sim, 2, function(x) lines(lowess(iGrid, x), lwd=0.5, col='red'))
+
+s2 = cbind(extract(fit.stan.pa)$betas)
+colnames(s2) = c(colnames(lStanData$X))
+dim(s2)
+pairs(s2[sample(1:nrow(s2), 1000),], pch=20, col='grey', cex=0.5, main='PA')
+
+
+# ## simulate the DAGs
+# # P.Sp.A -> P.Sh
+# df.Sim = data.frame(1:100)
+# df.Sim$Peanut.Sp.Act = rnorm(100)
+# df.Sim$Peanut.Shannon = rnorm(100, df.Sim$Peanut.Sp.Act)
+# df.Sim$ISAC.Shannon = rnorm(100)
+# df.Sim$CD63.Act = rnorm(100, df.Sim$Peanut.Sp.Act + df.Sim$Peanut.Shannon + df.Sim$ISAC.Shannon)
+# str(df.Sim)
+# 
+# fit.5 <- quap(
+#   alist(
+#     CD63.Act ~ dnorm(mu, sigmaPop),
+#     mu <- b0 + b1*Peanut.Shannon + b2*ISAC.Shannon + b3*Peanut.Sp.Act,
+#     b0 ~ dcauchy(0, 2),
+#     c(b1, b2, b3) ~ dnorm(0, 10),
+#     sigmaPop ~ dunif(1, 40)
+#   ), data=df.Sim,
+#   start=list(b0=21, b1=0, b2=0, b3=0)
+# )
+# summary(fit.5)
+# dfData.pa = df.Sim
+# dfData.pa = dfData.pa.bk
